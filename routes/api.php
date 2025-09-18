da20f6c72b6db7d1bb222e4efcf0d08c417c1c0c
Sibok <alfred0823@hotmail.com>
2025-09-18 14:38:43 -0600

    Added endpoints needed to interact with the map

--- routes/api.php
+++ routes/api.php
@@ -11,8 +11,22 @@ Route::get('/states/{id}',[StateController::class,'show']);
 Route::get('/municipalities',[MunicipalityController::class,'index']);
 Route::get('/municipalities/{id}',[MunicipalityController::class,'show']);
 
+Route::get('/categories',[MunicipalityController::class,'index']);
+Route::get('/categories/{id}',[MunicipalityController::class,'show']);
+Route::get('/categories/{id}',[MunicipalityController::class,'show']);
+Route::get('/categories/{id}/subcatgories',[MunicipalityController::class,'show']);
+
 Route::get('/layers',[LayerController::class,'index']);
-Route::post('/layers',[LayerController::class,'store']);
+Route::get('/layers/minimal',[LayerController::class,'index']);
+Route::get('/layers/countries',[LayerController::class,'getAllCountryLayers']);
+Route::get('/layers/countries/{countryId}',[LayerController::class,'getLayerByCountry']);
+Route::get('/layers/countries/{countryId}/states',[LayerController::class,'getAllStatesLayers']);
+Route::get('/layers/countries/{countryId}/states/{stateId}',[LayerController::class,'getLayersByState']);
+Route::get('/states/countries/{countryId}/municipalities',[LayerController::class,'getAllMunicipalitiesLayers']);
+Route::get('/countries/{countryId}/states/{stateId}/municipalities/{municipalityId}',[LayerController::class,'getLayersByMunicipality']);
+Route::get('/countries/{countryId}/states/{stateId}/municipalities/{municipalityId}/sections',[LayerController::class,'getAllsectionsLayers']);
+Route::get('/countries/{countryId}/states/{stateId}/municipalities/{municipalityId}/sections/{sectionId}',[LayerController::class,'getLayersBySection']);
+Route::post('/layers/{id}',[LayerController::class,'store']);
 Route::get('/layers/{id}',[LayerController::class,'show']);
 Route::delete('/layers/{id}',[LayerController::class,'destroy']);
 Route::get('/layers/{id}/download',[LayerController::class,'downloadFile']);
